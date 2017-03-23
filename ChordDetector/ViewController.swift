fd36a86cd24e7329b2ea7791d41a999c823b75da
cemolcay <ccemolcay@gmail.com>
2017-03-24 02:42:22 +0300

    Initial Commit

---
+++ ChordDetector/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChordDetector
+//
+//  Created by Cem Olcay on 24/03/2017.
+//  Copyright © 2017 cemolcay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
